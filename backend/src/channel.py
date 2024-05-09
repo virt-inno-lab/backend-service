820b22ca89d6e4393699a39a00d43a592d898ff4
y4cer <a.buchnev@innopolis.university>
2024-05-09 21:53:41 +0300

    add user group change

--- backend/src/channel.py
+++ backend/src/channel.py
@@ -5,7 +5,7 @@ iam_token = os.environ["IAM_TOKEN"]
 
 api_url = "iam.api.cloud.yandex.net:443"
 
-def create_client_channel(addr: str = api_url, token: str = iam_token) -> grpc.Channel:
+def create_client_channel(api_url: str = api_url, token: str = iam_token) -> grpc.Channel:
     # Call credential object will be invoked for every single RPC
     call_credentials = grpc.access_token_call_credentials(token)
     channel_credential = grpc.ssl_channel_credentials()
@@ -14,5 +14,5 @@ def create_client_channel(addr: str = api_url, token: str = iam_token) -> grpc.C
         channel_credential,
         call_credentials,
     )
-    channel = grpc.secure_channel(addr, composite_credentials)
+    channel = grpc.secure_channel(api_url, composite_credentials)
     return channel
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,29 +1,10 @@
-import yandex.cloud.iam.v1.user_account_service_pb2_grpc as stub_s
-import yandex.cloud.iam.v1.user_account_service_pb2 as user_acc
-
-import channel
-
-import yapassport_management
-
-ch = channel.create_client_channel()
-
-user_accound_id = "ajefmmfid6p5cssshaqm" # Vova user account
-req = user_acc.GetUserAccountRequest(user_account_id=user_accound_id)
-
-stub = stub_s.UserAccountServiceStub(ch)
-
-user = stub.Get(req)
-print(
-    user.id,
-    user.yandex_passport_user_account,
-)
+import move_user
 
 bozhinoski = "bozhinoski@yandex.ru"
 vova = "vovasst@yandex.ru"
+stepan = "tsepa.step@yandex.ru"
+i = "sanka.buchnev@ya.ru"
 
-user = yapassport_management.get_user_by_email(bozhinoski)
-
-print(
-    user.id,
-    user.yandex_passport_user_account,
-)
+move_user.update_cap(i, move_user.Cap.cloud_auditor)
+# ValueError because there is no such user!! woah!!
+move_user.update_cap(bozhinoski, move_user.Cap.cloud_auditor)
