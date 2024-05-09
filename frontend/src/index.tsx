271c1ae6531093cd47c66bd9d0bd1e19c2d4a3ef
MasterLogick <31408232+masterlogick@users.noreply.github.com>
2024-05-09 19:36:07 +0300

    add frontend initial setup

---
+++ frontend/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import "@mantine/core/styles.css";
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+
+root.render(
+    <React.StrictMode>
+        <h1>test</h1>
+    </React.StrictMode>
+);
