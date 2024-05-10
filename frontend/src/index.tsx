8a5b4936c2f42cdbf83d04d632b8a4fb80098fed
MasterLogick <31408232+masterlogick@users.noreply.github.com>
2024-05-10 12:45:11 +0300

    implement frontend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {App} from "./App";
 import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
 
 root.render(
     <React.StrictMode>
-        <h1>test</h1>
+        <App/>
     </React.StrictMode>
 );
