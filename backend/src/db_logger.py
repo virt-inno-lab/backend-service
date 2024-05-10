c9e2c80f94fb934ad982084a33bfea7128b80fbc
MasterLogick <31408232+masterlogick@users.noreply.github.com>
2024-05-10 17:09:02 +0300

    fix logger

--- backend/src/db_logger.py
+++ backend/src/db_logger.py
@@ -1,11 +1,9 @@
 import boto3
 import os
-import datetime
 import json
 
 LOGGING_SECRET_KEY = os.environ["LOGGING_SECRET_KEY"]
 LOGGING_SECRET_KEY_ID = os.environ["LOGGING_SECRET_KEY_ID"]
-
 
 
 def log(email, reason, new_cap):
@@ -24,5 +22,6 @@ def log(email, reason, new_cap):
     client.put_record(
         StreamName="/ru-central1/b1giekpmcv3no5p0g1d9/etnsro03ueudc94alh8o/log-stream",
         Data=json.dumps(log_entry_json), PartitionKey="escalation-log")
+
 
 log(email="a@example.com", reason="i want to die", new_cap="cloud.viewer")
